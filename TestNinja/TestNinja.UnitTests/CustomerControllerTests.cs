70c8bfabe353e2554fc7a18c7c2a8461fc56046d
lbegazo <luis_begazo@hotmail.es>
2018-05-11 09:49:36 -0500

    Controller test
    Controller test

---
+++ TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var customerController = new CustomerController();
+
+            var result = customerController.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var customerController = new CustomerController();
+
+            var result = customerController.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+    }
+}
